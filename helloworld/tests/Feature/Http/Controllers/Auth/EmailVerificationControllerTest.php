d759f9bb5298492f4ee785751759abb8001d4298
pshSunny <psh@100yearshop.co.kr>
2025-02-05 22:28:39 +0900

    이메일 인증 테스트4

--- helloworld/tests/Feature/Http/Controllers/Auth/EmailVerificationControllerTest.php
+++ helloworld/tests/Feature/Http/Controllers/Auth/EmailVerificationControllerTest.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\ValidateSignature;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,14 +15,14 @@ use Tests\TestCase;
 class EmailVerificationControllerTest extends TestCase
 {
     /**
-     * verification.notice:
+     * verification.notice: 이메일 인증되지 않았을 경우 진입
      */
     public function testReturnsVerifyEmailViewForUnverifiedUser(): void
     {
-        $this->withoutMiddleware(Authenticate::class)
+        $this->withoutMiddleware(Authenticate::class) // 사용자 인증 여부 검증하는 auth 미들웨어 제외
         ->get(route('verification.notice'))
         ->assertOk()
-        ->assertViewIs('auth.verify-email');
+        ->assertViewIs('auth.verify-email'); // 뷰 체크
     }
 
     /**
@@ -39,5 +41,21 @@ class EmailVerificationControllerTest extends TestCase
         ->assertRedirect(RouteServiceProvider::HOME);
 
         $this->assertTrue($user->hasVerifiedEmail()); // 사용자 이메일 검증여부 체크
+    }
+
+    /**
+     * verification.send: 이메일 재전송
+     */
+    public function testSendEmailForEmailVerification(): void
+    {
+        Notification::fake(); // 알림 전송 제한 Fake
+
+        $user = User::factory()->unverified()->create(); // 이메일 인증되지 않은 User 생성
+
+        $this->actingAs($user) // 로그인 된 것으로 간주
+        ->post(route('verification.send'))
+        ->assertRedirect();
+        
+        Notification::assertSentTo($user, VerifyEmail::class); // 알림 전송 검증
     }
 }
