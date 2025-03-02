418f7c9330d9c5c0a15607d8f477f3b2f5740c84
pshSunny <psh@100yearshop.co.kr>
2025-03-02 19:32:25 +0900

    헤더 UI 변경(bootstrap)

--- helloworld/resources/views/layouts/app.blade.php
+++ helloworld/resources/views/layouts/app.blade.php
@@ -5,35 +5,54 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name') }} - @yield('title', '^^')</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha256-PI8n5gCcz9cQqQXm3PEtDuPG8qx9oFsFctPg0S5zb8g=" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 pt-2">
-                    <div class="row">
-                        <div class="col-8">
-                            <h1 class="display-one"><a href="{{ url('/') }}">라라벨 가볍게 따라하기</a></h1>
-                            <h2>@yield('title')</h2>
-                        </div>
-                        <div class="col-4">
-                            @if (Auth::check())
-                                <form action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <button type="submit">로그아웃</button>
-                                </form>
-                                <a href="{{ route('profile.show') }}">개인정보 보기</a>
-                            @else
-                                <a href="{{ route('login') }}">로그인</a>
-                                <a href="{{ route('register') }}">회원가입</a>
-                            @endif
-                            <a href="{{ route('admin.dashboard') }}">어드민</a>
-                            <a href="{{ route('blog_index') }}">Blog</a>
-                        </div>
-                    </div>
-                    @yield('content')
+        <nav class="navbar navbar-dark bg-dark mb-3">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ url('/') }}">라라벨 가볍게 따라하기</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        @if (Auth::check())
+                        <li class="nav-item">
+                            <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="nav-link">로그아웃</button>
+                            </form>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('profile.show') }}" class="nav-link">개인정보 보기</a>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link">로그인</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('register') }}" class="nav-link">회원가입</a>
+                        </li>
+                        @endif
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ route('admin.dashboard') }}">어드민</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="{{ route('blog_index') }}">Blog</a></li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </nav>
+
+        <div class="container">
+            <h1>@yield('title')</h1>
+            @yield('content')
         </div>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha256-PI8n5gCcz9cQqQXm3PEtDuPG8qx9oFsFctPg0S5zb8g=" crossorigin="anonymous">
     </body>
 </html>
